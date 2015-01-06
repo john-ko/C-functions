63671f4342413cdca373b8b93b358feafad76ecc
john <johnko426@gmail.com>
2015-01-05 20:42:13 -0800

    added concat.c functions

---
+++ concat.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* concat_forloop(char* first, int first_size, char* second, int second_size)
+{
+	int size = first_size + second_size;
+	char * concat;
+	concat = malloc(first_size + second_size + 1);
+
+	printf("size of 1st: %d \n", first_size);
+	
+	for(int i = 0; i < first_size; i++)
+	{
+		concat[i] = first[i];		
+	}
+
+	for(int i = first_size; i < size+1; i++)
+	{
+		concat[i] = second[i - first_size + 1];
+	}
+	
+	return concat;
+}
+
+char* concat_malloc(char* first, int first_size, char* second, int second_size)
+{
+	int size = first_size + second_size;
+	char * concat;
+	concat = malloc(first_size + second_size + 1);
+	
+	memcpy(concat, first, first_size);
+	memcpy(concat + first_size, second, second_size);
+
+	return concat;
+}
+
+int main()
+{
+	char first[] = "Hello";
+	char second[] = "World!!";
+	char *concat_word1;
+	char *concat_word2;
+
+	concat_word1 = concat_forloop(first, sizeof(first)-1, second, sizeof(second)-1);
+	printf("new word is: %s \n", concat_word1);
+	
+
+	concat_word2 = concat_malloc(first, sizeof(first)-1, second, sizeof(second)-1);
+	printf("new word is: %s \n", concat_word2);
+	
+}
